8adfa8f9e31f6daa3f4fb634098a641608227299
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-18 23:25:24 +0530

    update: proxy backend url in client using vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173, // default Vite port
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
