aeebe70c8e1ff1166bce78e8fa4d3f94a934380f
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-19 10:29:16 +0530

    add better spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react";
+import { Spinner, Box } from "@chakra-ui/react";
 import { useListEvents } from "./hooks/reactQuery/useEvents";
 import { toIst } from "./utils/date";
 
@@ -6,7 +6,18 @@ function App() {
   const { data: { data } = {}, isLoading } = useListEvents();
 
   if (isLoading) {
-    return <h1>Loading</h1>;
+    return (
+      <Box minH="100vh" bg="gray.50" transition="all 0.2s">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minH="100vh"
+        >
+          <Spinner size="xl" />
+        </Box>
+      </Box>
+    );
   }
 
   return (
