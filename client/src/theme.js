7e32ed5f56fe4163eece7cf93645976a77342157
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-19 10:49:14 +0530

    add inter font

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,10 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const fonts = {
+  heading: "Inter, system-ui, sans-serif",
+  body: "Inter, system-ui, sans-serif",
+};
+
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, fonts });
 
 export default theme;
