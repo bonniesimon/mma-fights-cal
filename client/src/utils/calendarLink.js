8154863c3bcacb1ea5da5cf7935d8c758076e19b
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-19 20:29:21 +0530

    add logic to get gcal description with fights preview

--- client/src/utils/calendarLink.js
+++ client/src/utils/calendarLink.js
@@ -3,9 +3,35 @@ import { google } from "calendar-link";
 export const generateGoogleCalendarLink = (event) => {
   const calendarEvent = {
     title: event.title,
+    description: generateFightsDescription(event.fights),
     start: event.date,
     duration: [6, "hour"],
   };
 
   return google(calendarEvent);
+};
+
+const generateFightsDescription = (fights) => {
+  const mainEventFights = fights.filter((fight) => fight.main);
+  const nonMainEventFights = fights.filter((fight) => !fight.main);
+
+  let fightDescription = "Main Event:\n";
+
+  fightDescription = mainEventFights.reduce(
+    (resultStr, currentFight) =>
+      resultStr +
+      `\n${currentFight.fighterA.name} vs ${currentFight.fighterB.name}`,
+    fightDescription
+  );
+
+  fightDescription = fightDescription + "\n\nPrelims:\n";
+
+  fightDescription = nonMainEventFights.reduce(
+    (resultStr, currentFight) =>
+      resultStr +
+      `\n${currentFight.fighterA.name} vs ${currentFight.fighterB.name}`,
+    fightDescription
+  );
+
+  return fightDescription;
 };
