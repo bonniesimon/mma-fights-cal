55fc56ae723f7a80b9ca94cb7f7cb5c935b02f8f
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-19 10:42:38 +0530

    set dark mode theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App.jsx";
+import theme from "./theme.js";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
